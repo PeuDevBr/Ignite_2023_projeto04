d42de814dcccd19b4d2631c169ed240153e43e43
Flávio Renato <peubevbr@gmail.com>
2023-05-07 19:09:34 -0300

    Otimização de imagem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import { AppProps } from "next/app"
 import { globaStyles } from "../styles/global"
+import Image from "next/image";
+
+import logoImg from '../assets/Logo.svg'
+import { Container, Header } from "../styles/pages/App";
 
 globaStyles();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} width={150} height={150} alt=""/>
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
